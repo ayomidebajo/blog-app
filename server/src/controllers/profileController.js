bd45073105c083cd91c39967511d37956f3981ac
Ayomide <oluwashinabajo@gmail.com>
2022-01-03 15:52:52 +0100

    Prepare logic for view profile

--- server/src/controllers/profileController.js
+++ server/src/controllers/profileController.js
@@ -1,2 +1,15 @@
 //Todo add endpoint for viewing profile
+const pool = require("../../db/db");
 //Todo make research on adding token to headers after signin
+const viewProfile = async (req, res, next) => {
+  try {
+    let errors = [];
+    let username = req.body;
+    if (!username) {
+      errors.push({
+        message: "You don't have access, please login",
+      });
+    }
+    await pool.query(`SELECT * FROM users WHERE email = $1`);
+  } catch (error) {}
+};
