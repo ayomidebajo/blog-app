6f40a15ca3ba3b31fa806050815b61b2f8208934
Ayomide <oluwashinabajo@gmail.com>
2022-01-20 14:22:19 +0100

    fix errors in prod

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
+
+const sslRedirect = require("heroku-ssl-redirect");
 const cors = require("cors");
 const session = require("express-session");
 const path = require("path");
@@ -18,6 +20,7 @@ initializePassport(passport);
 const PORT = process.env.PORT || 5000;
 
 //middleware
+app.use(sslRedirect());
 app.use(cors());
 app.use(express.json());
 app.use(
