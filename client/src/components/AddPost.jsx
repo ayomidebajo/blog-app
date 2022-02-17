7d66588c0ecef72d538008eadb76bf55ab034a45
Ayomide <oluwashinabajo@gmail.com>
2022-02-17 18:37:03 +0100

    fix ui in addpost comp

--- client/src/components/AddPost.jsx
+++ client/src/components/AddPost.jsx
@@ -3,6 +3,7 @@ import { inject, observer } from "mobx-react";
 import { Editor, EditorState, RichUtils, convertToRaw } from "draft-js";
 import "draft-js/dist/Draft.css";
 import debounce from "lodash/debounce";
+import { Link } from "react-router-dom";
 
 @inject("post")
 // create post
@@ -112,6 +113,82 @@ class AddPost extends Component {
     return (
       <div>
         <div className="container">
+          <div className="navbar-custom__container" ref={this.navContainer}>
+            <div
+              className={`navbar-custom ${
+                this.state.isVisble ? "navbar-custom__active" : ""
+              }`}
+            >
+              <ul className="navbar-links__container">
+                <li className="navbar-link">
+                  <div className="logo">
+                    <em>
+                      Blog{" "}
+                      {console.log(
+                        this.state.isVisble ? "hell yeah" : "hell no"
+                      )}
+                    </em>
+                  </div>
+                </li>
+                <li className="navbar-link">
+                  <div className="mr-2">
+                    <svg
+                      width="13"
+                      height="16"
+                      viewBox="0 0 16 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8 20C9.1 20 10 19.1 10 18H6C6 19.1 6.89 20 8 20ZM14 14V9C14 5.93 12.36 3.36 9.5 2.68V2C9.5 1.17 8.83 0.5 8 0.5C7.17 0.5 6.5 1.17 6.5 2V2.68C3.63 3.36 2 5.92 2 9V14L0 16V17H16V16L14 14Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </div>
+                </li>
+
+                <li className="navbar-link dropdown">
+                  <div
+                    className="avatar dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-expanded="false"
+                  ></div>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <Link
+                      key="create-post"
+                      className="dropdown-item"
+                      to="/post"
+                    >
+                      Create post
+                    </Link>
+                    <Link key="dashboard" className="dropdown-item" to="#">
+                      Dashboard
+                    </Link>
+                    <Link
+                      to="#"
+                      key="change-username"
+                      className="dropdown-item"
+                    >
+                      Change Username
+                    </Link>
+                    <Link
+                      to="#"
+                      key="logout"
+                      className="dropdown-item"
+                      onClick={() => this.props.authStore.logOut()}
+                    >
+                      Logout
+                    </Link>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
           <div className="container-editor">
             <textarea
               name="title"
